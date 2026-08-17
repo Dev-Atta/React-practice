7bc463154c41a777f8f770c307d4182623488ba7
Atta <meatta001@gmail.com>
2026-08-18 01:03:47 +0500

    Updating workflow in vitereact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,11 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 
+
+import Atta from "./atta.jsx"
+
 function App() {
   const [count, setCount] = useState(0)
 
+
+ // in jsx WE can Only return one element 
   return (
-    <h1>Hello Atta, Vite + React!</h1>
+   // <h1>Hello Atta, Vite + React!</h1>
+   <Atta />
   )
 }
 
---
+++ 01vitereact/src/atta.jsx
@@ -0,0 +1,8 @@
+function Atta(){
+    return (
+        <h2>This is FROM atta.js</h2>
+    )
+       
+}
+
+export default Atta; // Exporting is Important to Import
